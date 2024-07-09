117ac005bd58d7fc78c93e0b820d3e07ff6a7d87
apenfe <adrian1414@hotmail.es>
2024-07-09 21:05:06 +0200

    interactividad

---
+++ hola_mundo/js/10-ventanas.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// alertas
+alert("mensaje de una alerta");
+
+// alertas de confirmacion
+var resultado = confirm("Esta seguro que desea continuar?");
+
+console.log(resultado);
+
+// ventana de ingreso de datos
+var edad = prompt("Que edad tienes?",18);
+console.log(parseInt(edad));
