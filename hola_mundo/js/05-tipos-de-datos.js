d71949376f2885fcd0ea3f6adec6845a68a21b2e
apenfe <adrian1414@hotmail.es>
2024-07-09 19:59:41 +0200

    variables

---
+++ hola_mundo/js/05-tipos-de-datos.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// operadores
+var num1 = 7;
+var num2 = 12;
+var operacion = num1 + num2;
+var operacion = num1 - num2;
+var operacion = num1 * num2;
+var operacion = num1 / num2;
+var operacion = num1 % num2;
+
+alert("El resultado de la operacion es "+operacion)
+
+// tipos de datos
+
+var numero_entero = 44;
+var cadena_texto = "Hola 'que' tal?";
+var verdadero_o_falso = true;
+
+var numero_falso = "33";
+var flotante_falso = "33.5"
+console.log(Number(numero_falso) + 7);
+console.log(parseInt(numero_falso));
+console.log(parseFloat(flotante_falso));
+
+String(numero_entero); // para convertir a string
+
