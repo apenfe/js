7453e526ad1ec643a319cedb65e294965a8f2192
apenfe <adrian1414@hotmail.es>
2024-07-10 14:57:32 +0200

    funciones en JS

---
+++ hola_mundo/js/24-plantillas.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var nombre = prompt("mete tu nombre");
+var apellidos = prompt("mete tus apellidos");
+
+var texto = `
+    <h1>Hola que tal</h1>
+    <h3>Mi nombre es: ${nombre}</h3>
+    <h3>Mis apellidos son: ${apellidos}</h3>
+    `;
+
+document.write(texto);
