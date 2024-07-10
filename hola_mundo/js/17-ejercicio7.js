6ce2726f4eb788ca4625a866abe69cfa6e49e1f7
apenfe <adrian1414@hotmail.es>
2024-07-10 13:34:39 +0200

    ejercicios js bloque 1

--- hola_mundo/js/17-ejercicio7.js
+++ hola_mundo/js/17-ejercicio7.js
@@ -2,3 +2,18 @@
 
 // tabla de multiplicar de un numero introducido por pantalla
 
+var numero = parseInt(prompt("Numero de tabla: "));
+
+for(let i=0; i<=10; i++){
+    console.log(i+" x "+numero+" = "+(numero*i));
+}
+
+for(let i=1; i<=10; i++){
+
+    document.write("<h2>Tabla del "+i+"</h2><br/>");
+
+    for(let j=0; j<=10; j++){
+
+        document.write("<h3>"+i+" x "+j+" = "+(j*i)+"</h3>");
+    }
+}
---
+++ hola_mundo/js/18-ejercicio8.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/*
+Calculadora:
+-pida dos numeros por pantalla
+-si metemos una mal que nos los vuelva a pedir
+- en el cuerpo de la pagina, en una alerta y por la consola el resultado de sumar, restar, multiplicar y dividir
+*/
+
+while(true){
+
+    var num1 = parseInt(prompt("Inserta el 1º numero: ",0));
+    var num2 = parseInt(prompt("Inserta el 2º numero: ",0));
+
+    if(isNaN(num1)||isNaN(num1)){
+
+        alert("Datos introducidos no son correctos");
+        continue;
+
+    }else{
+
+        alert("+: "+(num1+num2)+"\n, -: "+(num1-num2)+"\n, *: "+(num1*num2)+"\n, /: "+(num1/num2));
+        document.write("<h1>De "+num1+" a "+num2+" estas son las operaciones: </h1><br/>");
+        document.write("<h2>Suma: " + num1 + " + " + num2 + " = " + (num1+num2) + "</h2><br/>");
+        document.write("<h2>Resta: " + num1 + " - " + num2 + " = " + (num1-num2) + "</h2><br/>");
+        document.write("<h2>Multiplicacion: " + num1 + " * " + num2 + " = " + (num1*num2) + "</h2><br/>");
+        document.write("<h2>Division: " + num1 + " / " + num2 + " = " + (num1/num2) + "</h2><br/>");
+        break;
+
+    }
+
+}
