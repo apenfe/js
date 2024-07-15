a3f842b0c46606ef34a3907ba68e96541f3902f3
apenfe <adrian1414@hotmail.es>
2024-07-15 19:40:15 +0200

    JQuery UI interacciones

--- aprendiendo-jquery/js/06-interface.js
+++ aprendiendo-jquery/js/06-interface.js
@@ -1,6 +1,27 @@
 $(document).ready(function(){
 
+    // mover elemnto por la pagina
+    $('.elemento').draggable();
 
+    // redimensionar
+    $('.elemento').resizable();
 
+    // lista seleccionable 
+    $('.lista_seleccionable').selectable();
+
+    // lista ordenable 
+    $('.lista_ordenable').sortable({
+        update: function(event,ui){
+            console.log("cambio en la lista");
+        }
+    });
+
+    // drop
+    $('#elemento_movido').draggable();
+    $('#area').droppable({
+        drop: function(){
+            console.log("Has soltado algo dentro del area");
+        }
+    });
 
 });
